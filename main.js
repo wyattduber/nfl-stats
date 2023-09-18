494bf7212ac4ca49c480c3048fdedc2d356ec54d
DoubleYouCash <wyattduber@gmail.com>
2023-09-17 20:30:51 -0500

    fixed some css and some calculation stuff

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('darkModeToggle').addEventListener('click', function() {
         document.body.classList.toggle('dark-mode');
+    
+    // Hack to force a redraw
+    var display = document.body.style.display;
+    document.body.style.display = 'none';
+    document.body.offsetHeight; // no need to store this anywhere, the reference is enough
+    document.body.style.display = display;
     });
 });
 
--- passer-rating-calculator/script.js
+++ passer-rating-calculator/script.js
@@ -5,6 +5,10 @@ function calculateRating() {
     let touchdowns = parseFloat(document.getElementById('touchdowns').value);
     let interceptions = parseFloat(document.getElementById('interceptions').value);
 
+    if (!validateInput(attempts, completions, yards, touchdowns, interceptions)) {
+        return; // exit if validation fails
+    }
+
     let a = (completions / attempts - 0.3) * 5;
     let b = (yards / attempts - 3) * 0.25;
     let c = touchdowns / attempts * 20;
@@ -18,4 +22,32 @@ function calculateRating() {
     let rating = (a + b + c + d) / 6 * 100;
 
     document.getElementById('rating-result').innerText = rating.toFixed(1);
+}
+
+function validateInput(attempts, completions, yards, touchdowns, interceptions) {
+    let isValid = true;
+    
+    const inputIds = ['attempts', 'completions', 'yards', 'touchdowns', 'interceptions'];
+    const inputValues = [attempts, completions, yards, touchdowns, interceptions];
+    
+    for (let i = 0; i < inputIds.length; i++) {
+        if (isNaN(inputValues[i])) {
+            document.getElementById(inputIds[i]).style.borderColor = 'red';
+            isValid = false;
+        } else {
+            document.getElementById(inputIds[i]).style.borderColor = '';
+        }
+    }
+
+    if (completions > attempts || touchdowns > attempts || interceptions > attempts) {
+        alert("Completions, touchdowns, or interceptions cannot exceed passes attempted.");
+        isValid = false;
+    }
+
+    if (attempts < 0 || completions < 0 || yards < 0 || touchdowns < 0 || interceptions < 0) {
+        alert("Negative values are not allowed.");
+        isValid = false;
+    }
+
+    return isValid;
 }
